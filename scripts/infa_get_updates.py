8e954b4bda74a10b42eb639d8b1042f70cacd9e4
Rameswar-git1 <Rameswara.Reddy@safelite.com>
2025-05-30 16:30:11 +0530

    Update infa_get_updates.py

--- scripts/infa_get_updates.py
+++ scripts/infa_get_updates.py
@@ -1,4 +1,3 @@
-
 import requests
 import os
 import json
@@ -18,8 +17,8 @@ print('Getting all objects for the commit: ' + COMMIT_HASH)
 r = requests.get(URL + "/public/core/v3/commit/" + COMMIT_HASH, headers=HEADERS)
 
 if r.status_code != 200:
-    print("Exception caught: " + r.text)
-    sys.exit(99)
+    print("Exception caught: " + r.text)
+    sys.exit(99)
 
 request_json = r.json()
 
@@ -28,33 +27,33 @@ r_filtered = [x for x in request_json['changes'] if x['type'] == 'MTT']
 
 # This loop runs tests for each one of the mapping tasks
 for x in r_filtered:
-    BODY = {"@type": "job", "taskId": x['appContextId'], "taskType": "MTT"}
-    t = requests.post(URL + "/api/v2/job/", headers=HEADERS_V2, json=BODY)
+    BODY = {"@type": "job", "taskId": x['appContextId'], "taskType": "MTT"}
+    t = requests.post(URL + "/api/v2/job/", headers=HEADERS_V2, json=BODY)
 
-    if t.status_code != 200:
-        print("Exception caught: " + t.text)
-        sys.exit(99)
+    if t.status_code != 200:
+        print("Exception caught: " + t.text)
+        sys.exit(99)
 
-    test_json = t.json()
-    PARAMS = "?runId=" + str(test_json['runId'])
+    test_json = t.json()
+    PARAMS = "?runId=" + str(test_json['runId'])
 
-    STATE = 0
+    STATE = 0
 
-    while STATE == 0:
-        time.sleep(60)
-        a = requests.get(URL + "/api/v2/activity/activityLog" + PARAMS, headers=HEADERS_V2)
-        activity_log = a.json()
+    while STATE == 0:
+        time.sleep(60)
+        a = requests.get(URL + "/api/v2/activity/activityLog" + PARAMS, headers=HEADERS_V2)
+        activity_log = a.json()
 
-        if activity_log and 'state' in activity_log[0]:
-            STATE = activity_log[0]['state']
-        else:
-            print("Unexpected response format or missing 'state' key:", activity_log)
-            sys.exit(99)
+        if activity_log and 'state' in activity_log[0]:
+            STATE = activity_log[0]['state']
+        else:
+            print("Unexpected response format or missing 'state' key:", activity_log)
+            sys.exit(99)
 
-    if STATE != 1:
-        print("Mapping task: " + activity_log[0]['objectName'] + " failed.")
-        sys.exit(99)
-    else:
-        print("Mapping task: " + activity_log[0]['objectName'] + " completed successfully.")
+    if STATE != 1:
+        print("Mapping task: " + activity_log[0]['objectName'] + " failed.")
+        sys.exit(99)
+    else:
+        print("Mapping task: " + activity_log[0]['objectName'] + " completed successfully.")
 
 requests.post(URL + "/public/core/v3/logout", headers=HEADERS)
