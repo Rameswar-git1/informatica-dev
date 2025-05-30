3913439a42b5f9aef3244dcc5d293bbc40d2e0e5
Rameswar-git1 <Rameswara.Reddy@safelite.com>
2025-05-30 13:40:28 +0530

    Update infa_login.py

--- scripts/infa_login.py
+++ scripts/infa_login.py
@@ -9,11 +9,11 @@ import os
 # arising from any such use to the fullest extent permitted by law.
 
 URL = os.environ['IICS_LOGIN_URL']
-USERNAME = os.environ['IICS_USERNAME']
-PASSWORD = os.environ['IICS_PASSWORD']
+USERNAME = 'rameswara.reddy.dev'   #os.environ['IICS_USERNAME']
+PASSWORD = 'Harchana12#'   #os.environ['IICS_PASSWORD']
 
-UAT_USERNAME = os.environ['UAT_IICS_USERNAME']
-UAT_PASSWORD = os.environ['UAT_IICS_PASSWORD']
+UAT_USERNAME = 'us.qa.admin'  #os.environ['UAT_IICS_USERNAME']
+UAT_PASSWORD = 'Belron@0525'  #os.environ['UAT_IICS_PASSWORD']
 
 URL = "https://dm-us.informaticacloud.com/saas/public/core/v3/login"
 BODY = {"username": USERNAME,"password": PASSWORD}
